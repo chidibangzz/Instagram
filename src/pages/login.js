e588a4dbdc50ad52bc0403d2373fc397e0baf0d6
Chidi Ekeke <chidiekeke18@yahoo.com>
2021-04-26 19:01:00 -0500

    added pictures and form value using tailwind

--- src/pages/login.js
+++ src/pages/login.js
@@ -19,5 +19,30 @@ export default function Login() {
     document.title = 'Login - Instagram';
   }, []);
 
-  return <p>I am the default page</p>;
+  return (
+    <div
+      className="container flex mx-auto max-w-screen-md items-center
+    h-screen"
+    >
+      <div className="flex w-3/5">
+        <img src="/images/iphone-with-profile.jpeg" alt="iPhone with Instagram" />
+      </div>
+      <div className="flex flex-col w-2/5">
+        <h1 className="flex justify-center w-full">
+          <img src="/images/logo.png" alt="Instagram" className="mt-2 w-6/12 mb-4" />
+        </h1>
+
+        {error && <p className="mb-4 text-xs text-red-primary">{error}</p>}
+
+        <form onSubmit={handleLogin} method="POST">
+          <input
+            arial-label="Enter your email address"
+            type="text"
+            placeholder="Email Address"
+            className="text-sm text-gray-base"
+          />
+        </form>
+      </div>
+    </div>
+  );
 }
