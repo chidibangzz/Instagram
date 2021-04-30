1df41acfc0d3a7a0400420f6bd02880aa021b456
Chidi Ekeke <chidiekeke18@yahoo.com>
2021-04-30 02:25:51 -0500

    doubled nested array completed to get back images from user

--- src/components/timeline.js
+++ src/components/timeline.js
@@ -1,7 +1,26 @@
+/* eslint-disable no-nested-ternary */
+import Skeleton from 'react-loading-skeleton';
+import usePhotos from '../hooks/use-photos';
+
 export default function Timeline() {
+  // we need to get logged in users photos
+  const { photos } = usePhotos();
+  // on loading photos need to use react Skeleton
+  // if we have photos, render them(create a post component)
+  // if the user photo has no photos, tell them to create some photos
   return (
     <div className="container col-span-2">
-      <p> I am the timeline</p>
+      {!photos ? (
+        <>
+          {[...new Array(4)].map((_, index) => (
+            <Skeleton key={index} count={1} width={320} height={400} />
+          ))}
+        </>
+      ) : photos?.length > 0 ? (
+        photos.map((content) => <p key={content.docId}>{content.imageSrc}</p>)
+      ) : (
+        <p className="text-center text-2xl">Follow people to see photos</p>
+      )}
     </div>
   );
 }
